77f56609dd3496a29ea462dc8abd16c7d4532e9c
iamcleopatra_ <cynthiacleopatra03@gmail.com>
2024-11-16 19:42:06 +0200

    added index.js content

---
+++ Frontend/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Web3Provider } from './context/Web3Context';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Web3Provider>
+      <App />
+    </Web3Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
